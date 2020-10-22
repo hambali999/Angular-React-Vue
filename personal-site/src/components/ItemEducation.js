d40943882a75543f6a47b070b4d9ee36360b33a9
alexkimeu0 <kim3ual3x@gmail.com>
2020-10-22 12:14:50 +0300

    Added a template for a personal website

---
+++ personal-site/src/components/ItemEducation.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+const ItemEducation = () => {
+	return (
+        <div>
+            <div class="timeline-container">
+                <div class="content">
+                    <h3 className="job-title">
+                        Full Stack Web Development
+                        <span>Feb 2020 ~ Sep 2020</span>
+                    </h3>
+                    <h4>FreeCodeCamp</h4>
+                    <p>I completed the six certifications in FreeCodeCamp's Web Development curriculum. I completed a total of 30 projects, 5 for every section.</p>
+                </div>
+            </div>
+
+            <div class="timeline-container">
+                <div class="content">
+                    <h3 className="job-title">
+                        Cisco Routing & Switching
+                        <span>Sep 2015 ~ Dec 2017</span>
+                    </h3>
+                    <h4>Cisco Networking Academy</h4>
+                    <p>I studied Cisco Routing & Switching technologies, IP addressing and WAN technologies.</p>
+                </div>
+            </div>
+
+            <div class="timeline-container">
+                <div class="content">
+                    <h3 className="job-title">
+                        Bsc. IT
+                        <span>Sep 2014 ~ Nov 2018</span>
+                    </h3>
+                    <h4>Maseno University</h4>
+                    <p>Completed a Bsc. IT course. Studied Computer Programming essentials, Computer Networking, e.t.c</p>
+                </div>
+            </div>
+
+            <div class="timeline-container">
+                <div class="content">
+                    <h3 className="job-title">
+                        KCSE
+                    <span>Feb 2010 ~ Nov 2013</span>
+                    </h3>
+                    <h4>Jamhuri High School</h4>
+                    <p>Completed KCSE. Participated in Journalism and French club.</p>
+                </div>
+            </div>
+
+
+        </div>
+	);
+};
+
+export default ItemEducation;
