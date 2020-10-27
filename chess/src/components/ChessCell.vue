a6c08eb4adb5205e1c41d31474b7f6978c83cb01
Brandon Lyons <lyonsbp@umich.edu>
2020-10-27 17:04:47 -0400

    Add Vue 3 chess app

---
+++ chess/src/components/ChessCell.vue
@@ -0,0 +1,43 @@
+<template>
+  <div
+    class="symbol-text"
+    :class="{
+      'player-1': piece.owner === 1,
+      'player-2': piece.owner === 2,
+      active: piece.cellActive
+    }"
+    @click="$emit('cell-clicked', { row, col, piece })"
+  >
+    {{ piece.symbol }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    piece: Object,
+    row: Number,
+    col: Number
+  },
+  setup() {
+    return {};
+  }
+};
+</script>
+
+<style>
+.symbol-text {
+  font-weight: bold;
+}
+.player-1 {
+  color: red;
+}
+.player-2 {
+  color: black;
+}
+.active {
+  background-color: lightskyblue !important;
+  outline: 1px solid black;
+  outline-offset: -1px;
+}
+</style>
---
+++ chess/src/components/InfoBoard.vue
@@ -0,0 +1,13 @@
+<template>
+  <h3>Current Player: {{ gameState.currentPlayer }}</h3>
+</template>
+
+<script>
+export default {
+  props: {
+    gameState: Object
+  }
+};
+</script>
+
+<style></style>
